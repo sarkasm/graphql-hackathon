ea40cc2d784180289a95a14cf74098c660ffeca9
Robillard, Steven <srobillard@constantcontact.com>
2017-02-10 21:13:39 -0500

    combining

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,49 @@
 const graphFactory = require('./d3graph');
+const TypeCollection = require('./app');
+
+var nodes= [];
+var links= [];
+
+
+function getData(){
+  window.typeCollection = new TypeCollection();
+  window.typeCollection.fetch({success: function(){
+    window.typeCollection.each(function(m){
+      var relations = m.getRelations();
+      nodes.push(
+        {
+          'id': m.get('name')
+          // ,m.get('name')
+        }
+      );
+      _.each(relations, function(type){
+        // window.console.log(type);
+        links.push(
+          {
+            'source': m.id,
+            'target': type.get('name')
+          }
+        );
+      });
+      // nodes.push({m.id, m.get('name')});
+      // window.console.log(m.id);
+      // window.console.log(m.cid);
+      // window.console.log(m.getRelations());
+    });
+
+    var out = {
+      'nodes': nodes,
+      'links': links
+    };
+
+    graphFactory(out);
+
+    window.console.log(JSON.stringify(out));
+    // window.console.log(links);
+    // alert('got it');
+  }})
+}
+
 
 const mockData = {
   nodes: [
@@ -24,7 +69,8 @@ const mockData = {
 
 function init() {
   console.log('init');
-  graphFactory(mockData);
+  getData();
+  // graphFactory(mockData);
 }
 
 
