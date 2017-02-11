1f7a6a032b5933a82e292fed0be6a9d962c1b49c
Will Paul <wpaul@constantcontact.com>
2017-02-10 21:08:53 -0500

    Add field defintion support

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,51 @@
 const graphFactory = require('./d3graph');
+const renderTypeTable = require('./d3TypeTable');
 
 const mockData = {
   nodes: [
-    { id: 'Retweet' },
-    { id: 'SearchResponse' },
-    { id: 'Tweet' },
-    { id: 'TwitterAPI' },
-    { id: 'TwitterUser' },
-    { id: 'UserIdentifier' },
-    { id: 'UserIdentity' },
+    { id: 'Retweet', entity: 'type', fields: [
+      'id: ID',
+      'created_at: String',
+      'in_reply_to_tweet_id: String',
+      'in_reply_to_user_id: Int',
+      'in_reply_to_screen_name: String',
+      'retweeted_status: Tweet',
+      'user: TwitterUser',
+    ] },
+
+    { id: 'SearchResponse', entity: 'enum', fields: [ 'mixed', 'recent', 'popular' ] },
+
+    { id: 'Tweet', entity: 'type', fields: [
+      'id: ID',
+      'created_at: String',
+      'text: String',
+      'retweet_count: Int',
+      'user: TwitterUser',
+      'retweets(limit: Int = 5): [Retweet]',
+    ] },
+
+    { id: 'TwitterAPI', entity: 'type', fields: [
+      'user(identifier: UserIdentifier!, identity: UserIdentity!): TwitterUser',
+      'tweet(id: String!): Tweet',
+      'search(q: String!, count: Int, result_type: SearchReponse): [Tweet]',
+    ] },
+
+    { id: 'TwitterUser', entity: 'type', fields: [
+      'created_at: String',
+      'description: String',
+      'id: ID',
+      'screen_name: String',
+      'name: String',
+      'profile_image_url: String',
+      'url: String',
+      'tweets_count: Int',
+      'followers_count: Int',
+      'tweets(limit: Int = 10): [Tweet]',
+    ] },
+
+    { id: 'UserIdentifier', type: 'enum', fields: [ 'id', 'name', ] },
+
+    { id: 'UserIdentity', type: 'scalar', fields: [ ] },
   ],
   links: [
     { source: 'Retweet', target: 'Tweet', field: 'retweeted_status' },
@@ -23,9 +60,8 @@ const mockData = {
 }
 
 function init() {
-  console.log('init');
   graphFactory(mockData);
+  renderTypeTable(mockData.nodes[0]);
 }
-
 
 window.onload = init;
